1fc486dbd6dc5e8b945425c7e4ff960debc79216
Matthew Shampine <mshampine4@gmail.com>
2017-12-14 18:44:13 -0500

    Changed print to display

--- cash_register.scm
+++ cash_register.scm
@@ -1,14 +1,11 @@
 (define tax_rate 0.065)
-
 (define (start)
-    (print "Scheme Point-of-Sale")
-    (print "Start transaction (exit with 0):\n")
+    (display "Scheme Point-of-Sale")
+    (display "Start transaction (exit with 0):\n")
     (getValue 0.0)
 )
 
 (define (getValue total)
-    (print "$" total)
-    (print "")
     (display "Enter Value:")
     (let ((num (convertFloat (format "~a" (read)))))
         (cond ((equal? num 0) (quitProgram total))
@@ -22,10 +19,10 @@
 )
 
 (define (quitProgram total)
-    (print "\nSubtotal: $" total)
+    (display "\nSubtotal: $" total)
     (let ((tax (calculateTax total)))
-        (print "Tax: $" tax)
-        (print "Total: $" (+ total tax))
+        (display "Tax: $" tax)
+        (display "Total: $" (+ total tax))
     )
 )
 
